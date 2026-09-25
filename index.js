4109e938085e966cb1d641e91cfa3c2f9cfedc49
Harry-clue <harrisonnganga760@gmail.com>
2026-09-25 16:53:19 +0300

    used different functions in javascript

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-
+function calculateTax(amount, taxRate) {
+  return amount * (10/ 100);
+}
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
+function findMaximum(arr) {
+  return Math.max(arr);
+}
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discountAmount;
+}
 
 
 
